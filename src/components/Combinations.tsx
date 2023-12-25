07bb1114375288b7da67a8302a4bee041ea6cf87
Axxxll <alex.plam.atan@gmail.com>
2023-12-25 21:44:55 +0200

    Display of the combos is easier to understand

--- src/components/Combinations.tsx
+++ src/components/Combinations.tsx
@@ -1,8 +1,11 @@
 export default function Combinations(props: {combo: string}) {
+
+    const combo = props.combo.split(/(?=[A-Z])/).map((element, index) => index === 0 ? element[0].toUpperCase() + element.substring(1) : element[0].toLocaleLowerCase() + element.substring(1)).join(" ")
+    
     return (
-        <div className="combo-face">
+        <div className="combo-face" style={{backgroundColor: 'white'}}>
             <p className="combo">
-                {props.combo}
+                {combo}
             </p>
         </div>
     )
