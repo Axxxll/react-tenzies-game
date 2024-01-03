0acd219dbdc2517cdff4edb936b8692e81137634
Axxxll <alex.plam.atan@gmail.com>
2024-01-04 01:25:28 +0200

    Clean up of App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { nanoid } from 'nanoid'
 
 import getCombinations from './shared/getCombinations'
