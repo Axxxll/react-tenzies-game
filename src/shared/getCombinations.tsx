559824062ca7cd22c39ece2ae08f1983af2332fb
Axxxll <alex.plam.atan@gmail.com>
2024-01-11 16:43:47 +0200

    Changed selected to be IsSelected for better understanding that it is a boolean

--- src/shared/getCombinations.tsx
+++ src/shared/getCombinations.tsx
@@ -4,26 +4,26 @@ import { nanoid } from 'nanoid'
 export default function getCombinations() {
     return {
         mandatory: [
-            {combo: "setOfOne", comboState: comboState.NotUsed, selected: false, id: nanoid()},
-            {combo: "setOfTwo", comboState: comboState.NotUsed, selected: false, id: nanoid()},
-            {combo: "setOfThree", comboState: comboState.NotUsed, selected: false, id: nanoid()},
-            {combo: "setOfFour", comboState: comboState.NotUsed, selected: false, id: nanoid()},
-            {combo: "setOfFive", comboState: comboState.NotUsed, selected: false, id: nanoid()},
-            {combo: "setOfSix", comboState: comboState.NotUsed, selected: false, id: nanoid()}
+            {combo: "setOfOne", comboState: comboState.NotUsed, isSelected: false, id: nanoid()},
+            {combo: "setOfTwo", comboState: comboState.NotUsed, isSelected: false, id: nanoid()},
+            {combo: "setOfThree", comboState: comboState.NotUsed, isSelected: false, id: nanoid()},
+            {combo: "setOfFour", comboState: comboState.NotUsed, isSelected: false, id: nanoid()},
+            {combo: "setOfFive", comboState: comboState.NotUsed, isSelected: false, id: nanoid()},
+            {combo: "setOfSix", comboState: comboState.NotUsed, isSelected: false, id: nanoid()}
         ],
         extra: 
         [
-            {combo: "pair", comboState: comboState.NotUsed, selected: false, id: nanoid()},
-            {combo: "threeOfAKind", comboState: comboState.NotUsed, selected: false, id: nanoid()},
-            {combo: "fourOfAKind", comboState: comboState.NotUsed, selected: false, id: nanoid()},
-            {combo: "twoPlusTwo", comboState: comboState.NotUsed, selected: false, id: nanoid()},
-            {combo: "twoPlusThree", comboState: comboState.NotUsed, selected: false, id: nanoid()},
-            {combo: "oneToFive", comboState: comboState.NotUsed, selected: false, id: nanoid()},
-            {combo: "twoToSix", comboState: comboState.NotUsed, selected: false, id: nanoid()},
-            {combo: "odd", comboState: comboState.NotUsed, selected: false, id: nanoid()},
-            {combo: "even", comboState: comboState.NotUsed, selected: false, id: nanoid()},
-            {combo: "chance", comboState: comboState.NotUsed, selected: false, id: nanoid()},
-            {combo: "general", comboState: comboState.NotUsed, selected: false, id: nanoid()},
+            {combo: "pair", comboState: comboState.NotUsed, isSelected: false, id: nanoid()},
+            {combo: "threeOfAKind", comboState: comboState.NotUsed, isSelected: false, id: nanoid()},
+            {combo: "fourOfAKind", comboState: comboState.NotUsed, isSelected: false, id: nanoid()},
+            {combo: "twoPlusTwo", comboState: comboState.NotUsed, isSelected: false, id: nanoid()},
+            {combo: "twoPlusThree", comboState: comboState.NotUsed, isSelected: false, id: nanoid()},
+            {combo: "oneToFive", comboState: comboState.NotUsed, isSelected: false, id: nanoid()},
+            {combo: "twoToSix", comboState: comboState.NotUsed, isSelected: false, id: nanoid()},
+            {combo: "odd", comboState: comboState.NotUsed, isSelected: false, id: nanoid()},
+            {combo: "even", comboState: comboState.NotUsed, isSelected: false, id: nanoid()},
+            {combo: "chance", comboState: comboState.NotUsed, isSelected: false, id: nanoid()},
+            {combo: "general", comboState: comboState.NotUsed, isSelected: false, id: nanoid()},
 
 
         ]
