1808a939ba3ace0988f04aaae637717f79596391
Axxxll <alex.plam.atan@gmail.com>
2024-01-11 17:04:51 +0200

    Adding logic to apply styles for selected component

--- src/components/Combinations.tsx
+++ src/components/Combinations.tsx
@@ -4,11 +4,13 @@ export default function Combinations(props: { combo: string, comboState: number,
 
     const combo = props.combo.split(/(?=[A-Z])/).map((element, index) => index === 0 ? element[0].toUpperCase() + element.substring(1) : element[0].toLocaleLowerCase() + element.substring(1)).join(" ")
 
-    const classes = props.comboState === comboState.NotUsed ? 'not-used' : (props.comboState === comboState.Scrached ? 'scrached' : (props.comboState === comboState.Used ? 'used' : ""))
+    const comboStateClass = props.comboState === comboState.NotUsed ? 'not-used' : (props.comboState === comboState.Scrached ? 'scrached' : (props.comboState === comboState.Used ? 'used' : ""))
+
+    const selectedClass = props.isSelected ? "selected" : ""
     
 
     return (
-        <button className={`combo-face ${classes}`}  disabled={props.comboState === comboState.Scrached || props.comboState === comboState.Used} onClick={props.select}>
+        <button className={`combo-face ${comboStateClass} ${selectedClass}`}  disabled={props.comboState === comboState.Scrached || props.comboState === comboState.Used} onClick={props.select}>
             <p className="combo">
                 {combo}
             </p>
